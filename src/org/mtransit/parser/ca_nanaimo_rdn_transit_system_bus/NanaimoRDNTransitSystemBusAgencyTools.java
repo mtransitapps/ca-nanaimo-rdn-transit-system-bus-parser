ee95f3f6a5125274727899e3f2982f9570a6afdd
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 17:22:17 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_nanaimo_rdn_transit_system_bus/NanaimoRDNTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_nanaimo_rdn_transit_system_bus/NanaimoRDNTransitSystemBusAgencyTools.java
@@ -36,7 +36,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating RDN Transit System bus data...\n");
+		System.out.printf("\nGenerating RDN Transit System bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
