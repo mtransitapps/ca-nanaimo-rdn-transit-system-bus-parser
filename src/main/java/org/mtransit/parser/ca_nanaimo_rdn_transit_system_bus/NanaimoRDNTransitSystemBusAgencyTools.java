02a96924de46fb22c24b44cfb1cfd142e450afc5
Mathieu Méa <mathieu.mea@gmail.com>
2020-02-19 17:47:50 -0500

    Code cleaning

--- src/main/java/org/mtransit/parser/ca_nanaimo_rdn_transit_system_bus/NanaimoRDNTransitSystemBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_nanaimo_rdn_transit_system_bus/NanaimoRDNTransitSystemBusAgencyTools.java
@@ -1,17 +1,10 @@
 package org.mtransit.parser.ca_nanaimo_rdn_transit_system_bus;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang3.StringUtils;
 import org.mtransit.commons.StrategicMappingCommons;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.MTLog;
 import org.mtransit.parser.Pair;
 import org.mtransit.parser.SplitUtils;
 import org.mtransit.parser.SplitUtils.RouteTripSpec;
@@ -27,6 +20,14 @@ import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
 import org.mtransit.parser.mt.data.MTripStop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
 
 // https://bctransit.com/*/footer/open-data
 // https://bctransit.com/servlet/bctransit/data/GTFS - Nanaimo
@@ -47,11 +48,11 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating RDN Transit System bus data...");
+		MTLog.log("Generating RDN Transit System bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this, true);
 		super.start(args);
-		System.out.printf("\nGenerating RDN Transit System bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+		MTLog.log("Generating RDN Transit System bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
 
 	@Override
@@ -76,6 +77,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	private static final String INCLUDE_AGENCY_ID;
+
 	static {
 		INCLUDE_AGENCY_ID = "1"; // RDN Transit System only
 	}
@@ -106,7 +108,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		return Long.parseLong(gRoute.getRouteShortName()); // use route short name as route ID
 	}
 
-	private static final Pattern STARTS_WITH_DASH = Pattern.compile("(^\\- )", Pattern.CASE_INSENSITIVE);
+	private static final Pattern STARTS_WITH_DASH = Pattern.compile("(^- )", Pattern.CASE_INSENSITIVE);
 
 	@Override
 	public String getRouteLongName(GRoute gRoute) {
@@ -131,6 +133,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	private static final String ROUTE_COLOR_LOCAL = "809699";
 	private static final String ROUTE_COLOR_FREQUENT = "009FC2";
 
+	@SuppressWarnings("DuplicateBranchesInSwitch")
 	@Override
 	public String getRouteColor(GRoute gRoute) {
 		if (StringUtils.isEmpty(gRoute.getRouteColor())) {
@@ -160,8 +163,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 				if (isGoodEnoughAccepted()) {
 					return AGENCY_COLOR_BLUE;
 				}
-				System.out.printf("\nUnexpected route color for %s!\n", gRoute);
-				System.exit(-1);
+				MTLog.logFatal("Unexpected route color for %s!", gRoute);
 				return null;
 			}
 		}
@@ -206,37 +208,37 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	private static final String BC_FERRIES = "BC Ferries";
 	private static final String LANTZVILLE = "Lantzville";
 
-
 	private static HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
+
 	static {
-		HashMap<Long, RouteTripSpec> map2 = new HashMap<Long, RouteTripSpec>();
+		HashMap<Long, RouteTripSpec> map2 = new HashMap<>();
 		map2.put(11L, new RouteTripSpec(11L, //
 				StrategicMappingCommons.CLOCKWISE_0, MTrip.HEADSIGN_TYPE_STRING, "West", //
 				StrategicMappingCommons.CLOCKWISE_1, MTrip.HEADSIGN_TYPE_STRING, WOODGROVE) //
 				.addTripSort(StrategicMappingCommons.CLOCKWISE_0, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("109925"), // Woodgrove Centre Exchange Bay D
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("109925"), // Woodgrove Centre Exchange Bay D
 								Stops.getALL_STOPS().get("110220"), // ++
-								Stops.getALL_STOPS().get("110226"), // Eastwind at Northwind
-						})) //
+								Stops.getALL_STOPS().get("110226") // Eastwind at Northwind
+						)) //
 				.addTripSort(StrategicMappingCommons.CLOCKWISE_1, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("110226"), // Eastwind at Northwind
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("110226"), // Eastwind at Northwind
 								Stops.getALL_STOPS().get("109829"), // == Dover at Applecross
 								Stops.getALL_STOPS().get("109830"), // != Uplands at McRobb
 								Stops.getALL_STOPS().get("109831"), // != Nanaimo Seniors Village
 								Stops.getALL_STOPS().get("109929"), // != Dover at Uplands
 								Stops.getALL_STOPS().get("109921"), // != Dover Bay High School
 								Stops.getALL_STOPS().get("109922"), // == Hammond Bay at Uplands
-								Stops.getALL_STOPS().get("109925"), // Woodgrove Centre Exchange Bay D
-						})) //
+								Stops.getALL_STOPS().get("109925") // Woodgrove Centre Exchange Bay D
+						)) //
 				.compileBothTripSort());
 		map2.put(25L, new RouteTripSpec(25L, //
 				StrategicMappingCommons.CLOCKWISE_0, MTrip.HEADSIGN_TYPE_STRING, WOODGROVE, // VI_UNIVERSITY_SHORT
 				StrategicMappingCommons.CLOCKWISE_1, MTrip.HEADSIGN_TYPE_STRING, BC_FERRIES) //
 				.addTripSort(StrategicMappingCommons.CLOCKWISE_0, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("109880"), // == Departure Bay Ferry
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("109880"), // == Departure Bay Ferry
 								Stops.getALL_STOPS().get("109964"), // !== Stewart at Maple
 								Stops.getALL_STOPS().get("109872"), // ========== Front at Gabriola Ferry Term
 								Stops.getALL_STOPS().get("109873"), // !== Front at Esplanade
@@ -248,69 +250,69 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 								Stops.getALL_STOPS().get("109881"), // !== Brechin at Beach
 								Stops.getALL_STOPS().get("110215"), // !== Norwell at Victoria
 								Stops.getALL_STOPS().get("110006"), // == Metral at Enterprise
-								Stops.getALL_STOPS().get("109925"), // Woodgrove Centre Exchange Bay D
-						})) //
+								Stops.getALL_STOPS().get("109925") // Woodgrove Centre Exchange Bay D
+						)) //
 				.addTripSort(StrategicMappingCommons.CLOCKWISE_1, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("109925"), // Woodgrove Centre Exchange Bay D
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("109925"), // Woodgrove Centre Exchange Bay D
 								Stops.getALL_STOPS().get("110516"), // ++ Country Club Exchange Bay A
-								Stops.getALL_STOPS().get("109880"), // Departure Bay Ferry
-						})) //
+								Stops.getALL_STOPS().get("109880") // Departure Bay Ferry
+						)) //
 				.compileBothTripSort());
 		map2.put(88L, new RouteTripSpec(88L, //
 				StrategicMappingCommons.OUTBOUND_0, MTrip.HEADSIGN_TYPE_STRING, "Parksville", //
 				StrategicMappingCommons.OUTBOUND_1, MTrip.HEADSIGN_TYPE_STRING, "Wembley Mall") //
 				.addTripSort(StrategicMappingCommons.OUTBOUND_0, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("110299"), // Jensen Ave E at Craig
-								Stops.getALL_STOPS().get("110441"), // Pym St N at Jenkins
-								Stops.getALL_STOPS().get("104168"), // Wembley Mall AT Wembley Rd
-						})) //
-				.addTripSort(StrategicMappingCommons.OUTBOUND_1, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("104168"), // Wembley Mall AT Wembley Rd
-								Stops.getALL_STOPS().get("110280"), // Finholm St S at Morison
+						Arrays.asList(//
 								Stops.getALL_STOPS().get("110299"), // Jensen Ave E at Craig
-						})) //
+								Stops.getALL_STOPS().get("110441"), // Pym St N at Jenkins
+								Stops.getALL_STOPS().get("104168") // Wembley Mall AT Wembley Rd
+						)) //
+				.addTripSort(StrategicMappingCommons.OUTBOUND_1, //
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("104168"), // Wembley Mall AT Wembley Rd
+								Stops.getALL_STOPS().get("110280"), // Finholm St S at Morison
+								Stops.getALL_STOPS().get("110299") // Jensen Ave E at Craig
+						)) //
 				.compileBothTripSort());
 		map2.put(97L, new RouteTripSpec(97L, //
 				StrategicMappingCommons.COUNTERCLOCKWISE_0, MTrip.HEADSIGN_TYPE_STRING, "Ravensong", // "West", //
 				StrategicMappingCommons.COUNTERCLOCKWISE_1, MTrip.HEADSIGN_TYPE_STRING, "East") // Eaglecrest") //
 				.addTripSort(StrategicMappingCommons.COUNTERCLOCKWISE_0, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("110376"), // Eastbound Sunrise at Drew
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("110376"), // Eastbound Sunrise at Drew
 								Stops.getALL_STOPS().get("104080"), // Westbound Pintail at Eaglecrest Dr
 								Stops.getALL_STOPS().get("104113"), // Southbound Eaglecrest farside Mallard
 								Stops.getALL_STOPS().get("104122"), // ++
-								Stops.getALL_STOPS().get("110358"), // Southbound Jones at Fern Rd W
-						})) //
+								Stops.getALL_STOPS().get("110358") // Southbound Jones at Fern Rd W
+						)) //
 				.addTripSort(StrategicMappingCommons.COUNTERCLOCKWISE_1, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("110358"), // Southbound Jones at Fern Rd W
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("110358"), // Southbound Jones at Fern Rd W
 								Stops.getALL_STOPS().get("104060"), // ++
 								Stops.getALL_STOPS().get("104061"), // ++
-								Stops.getALL_STOPS().get("110376"), // Eastbound Sunrise at Drew
-						})) //
+								Stops.getALL_STOPS().get("110376") // Eastbound Sunrise at Drew
+						)) //
 				.compileBothTripSort());
 		map2.put(98L, new RouteTripSpec(98L, //
 				StrategicMappingCommons.CLOCKWISE_0, MTrip.HEADSIGN_TYPE_STRING, "Ravensong", //
 				StrategicMappingCommons.CLOCKWISE_1, MTrip.HEADSIGN_TYPE_STRING, "Island Hwy W") // "Qualicum Beach") //
 				.addTripSort(StrategicMappingCommons.CLOCKWISE_0, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("104141"), // Westbound Island Hwy W at 2711 Blk
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("104141"), // Westbound Island Hwy W at 2711 Blk
 								Stops.getALL_STOPS().get("104146"), // Westbound Island Hwy W ACR Beach Dr
 								Stops.getALL_STOPS().get("104147"), // == Southbound Garrett at Parkridge
 								Stops.getALL_STOPS().get("104138"), // != Southbound Garrett at Garrett Turn-About
 								Stops.getALL_STOPS().get("104149"), // == Eastbound Canyon at 727
-								Stops.getALL_STOPS().get("110358"), // Southbound Jones at Fern Rd W
-						})) //
+								Stops.getALL_STOPS().get("110358") // Southbound Jones at Fern Rd W
+						)) //
 				.addTripSort(StrategicMappingCommons.CLOCKWISE_1, //
-						Arrays.asList(new String[] { //
-						Stops.getALL_STOPS().get("110358"), // Southbound Jones at Fern Rd W
+						Arrays.asList(//
+								Stops.getALL_STOPS().get("110358"), // Southbound Jones at Fern Rd W
 								Stops.getALL_STOPS().get("104134"), // ++
 								Stops.getALL_STOPS().get("104140"), // Eastbound Crescent Rd W at Memorial
-								Stops.getALL_STOPS().get("104141"), // Westbound Island Hwy W at 2711 Blk
-						})) //
+								Stops.getALL_STOPS().get("104141") // Westbound Island Hwy W at 2711 Blk
+						)) //
 				.compileBothTripSort());
 		ALL_ROUTE_TRIPS2 = map2;
 	}
@@ -333,7 +335,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 			if (Arrays.asList( //
 					WESTWOOD, //
 					DOWNTOWN //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(DOWNTOWN, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -341,7 +343,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 			if (Arrays.asList( //
 					COUNTRY_CLUB, //
 					DOWNTOWN //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(DOWNTOWN, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -350,7 +352,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 					DOWNTOWN, //
 					CINNABAR, //
 					CINNABAR_CEDAR //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(CINNABAR_CEDAR, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -358,7 +360,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 			if (Arrays.asList( //
 					BC_FERRIES, //
 					LANTZVILLE //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(LANTZVILLE, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -368,14 +370,14 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 					VI_UNIVERSITY_SHORT + " Only", //
 					VI_UNIVERSITY_SHORT + "-", //
 					VI_UNIVERSITY_SHORT //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(VI_UNIVERSITY_SHORT, mTrip.getHeadsignId()); //
 				return true;
 			}
 			if (Arrays.asList( //
 					"A " + WOODGROVE, //
 					WOODGROVE //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(WOODGROVE, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -383,7 +385,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 			if (Arrays.asList( //
 					COUNTRY_CLUB, // <>
 					DOWNTOWN // <>
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(DOWNTOWN, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -391,7 +393,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 					DOWNTOWN, // <>
 					COUNTRY_CLUB, // <>
 					WOODGROVE //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(WOODGROVE, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -406,14 +408,14 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 			if (Arrays.asList( //
 					"School Special", //
 					WOODGROVE //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(WOODGROVE, mTrip.getHeadsignId()); //
 				return true;
 			}
 			if (Arrays.asList( //
 					VI_UNIVERSITY_SHORT + " Only", //
 					DOWNTOWN //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(DOWNTOWN, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -421,7 +423,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 			if (Arrays.asList( //
 					BC_FERRIES, //
 					WOODGROVE //
-					).containsAll(headsignsValues)) {
+			).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(WOODGROVE, mTrip.getHeadsignId()); //
 				return true;
 			}
@@ -434,18 +436,17 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 				return true;
 			}
 		}
-		System.out.printf("\nUnexpected trips to merge %s & %s!\n", mTrip, mTripToMerge);
-		System.exit(-1);
+		MTLog.logFatal("Unexpected trips to merge %s & %s!", mTrip, mTripToMerge);
 		return false;
 	}
 
-	private static final Pattern EXCHANGE_ = Pattern.compile("((^|\\W){1}(exchange)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern EXCHANGE_ = Pattern.compile("((^|\\W)(exchange)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String EXCHANGE_REPLACEMENT = "$2" + EXCHANGE_SHORT + "$4";
 
-	private static final Pattern VI_UNIVERSITY_ = Pattern.compile("((^|\\W){1}(vi university|viu)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern VI_UNIVERSITY_ = Pattern.compile("((^|\\W)(vi university|viu)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String VI_UNIVERSITY_REPLACEMENT = "$2" + VI_UNIVERSITY_SHORT + "$4";
 
-	private static final Pattern STARTS_WITH_NUMBER = Pattern.compile("(^[\\d]+( \\-)?[\\S]*)", Pattern.CASE_INSENSITIVE);
+	private static final Pattern STARTS_WITH_NUMBER = Pattern.compile("(^[\\d]+( -)?[\\S]*)", Pattern.CASE_INSENSITIVE);
 
 	private static final Pattern ENDS_WITH_VIA = Pattern.compile("( via .*$)", Pattern.CASE_INSENSITIVE);
 	private static final Pattern STARTS_WITH_TO = Pattern.compile("(^.* to )", Pattern.CASE_INSENSITIVE);
@@ -455,13 +456,13 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	private static final Pattern CLEAN_P2 = Pattern.compile("[\\s]*\\)[\\s]*");
 	private static final String CLEAN_P2_REPLACEMENT = ") ";
 
-	private static final Pattern BEACH_ = Pattern.compile("((^|\\W){1}(Bch)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern BEACH_ = Pattern.compile("((^|\\W)(Bch)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String BEACH_REPLACEMENT = "$2" + BEACH + "$4";
 
-	private static final Pattern CINNABAR_ = Pattern.compile("((^|\\W){1}(cinnibar)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern CINNABAR_ = Pattern.compile("((^|\\W)(cinnibar)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String CINNABAR_REPLACEMENT = "$2" + CINNABAR + "$4";
 
-	private static final Pattern SHUTTLE_ = Pattern.compile("((^|\\W){1}(shutlle)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final Pattern SHUTTLE_ = Pattern.compile("((^|\\W)(shutlle)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String SHUTTLE_REPLACEMENT = "$2" + "Shuttle" + "$4";
 
 	@Override
@@ -485,7 +486,7 @@ public class NanaimoRDNTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
 
-	private static final Pattern STARTS_WITH_IMPL = Pattern.compile("(^(\\(\\-IMPL\\-\\)))", Pattern.CASE_INSENSITIVE);
+	private static final Pattern STARTS_WITH_IMPL = Pattern.compile("(^(\\(-IMPL-\\)))", Pattern.CASE_INSENSITIVE);
 	private static final Pattern STARTS_WITH_BOUND = Pattern.compile("(^(east|west|north|south)(bound|boudn))", Pattern.CASE_INSENSITIVE);
 
 	@Override
